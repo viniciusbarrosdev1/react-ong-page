134967060229584f998f1f6f90cf6fd5ea5c7054
viniciusbarrosdev1 <viniciusbarrosdev@gmail.com>
2026-08-05 22:10:18 -0300

    wip home

--- src/pages/Animais.tsx
+++ src/pages/Animais.tsx
@@ -1,12 +1,9 @@
 import animais from "../data/animais.json";
-import AnimalCard from "../components/AnimalCard";
 
 function Animais() {
   return (
-    <div>
-      {animais.map((animal) => (
-        <AnimalCard key={animal.id} animal={animal} />
-      ))}
+    <div className="">
+
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import Header from "../components/Header";
+import HeroDog from "../assets/hero/hero-dog.png";
 
 function Home() {
   return (
-    <>
-      <h1>pagina inicial</h1>
-    </>
+    <div className="bg-[var(--cor-primaria)] flex items-center min-h-screen">
+      <div className="flex-1">
+        <h1 className="text-5xl font-bold">TEXTO HERO DE TESTE</h1>
+        <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quia consequuntur quasi sapiente culpa vel dicta officia incidunt dolor voluptas. Minus illo distinctio nostrum nam minima debitis sit deleniti sapiente.</p>
+      </div>
+
+      <div className="flex-1 flex justify-end items-end">
+        <img src={HeroDog} alt="Cachorro" />
+      </div>
+    </div>
   );
 }
 
